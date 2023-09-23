21f8379ab56dd5ef6f01cfcb8996ecd193be915a
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-23 10:14:43 -0700

    0x1B: Header File including Selection Sort funcs.

--- sort.h
+++ sort.h
@@ -1,8 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,5 +22,8 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void lumoto(int *array, int low, int high, size_t size);
 
 #endif

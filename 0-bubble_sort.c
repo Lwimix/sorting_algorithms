e13bb96b8aa18d2126cb0d20b1327e6abe9cf77a
Lwimix <martilwinx@gmail.com>
2023-09-24 09:04:02 +0300

    Bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,9 +14,9 @@ void bubble_sort(int *array, size_t size)
 
 	if (array == NULL || size <= 0 || size < 2)
 		return;
-	for (i = 0; i < size -1; ++i)
+	for (i = 0; i < size; ++i)
 	{
-		for (j = 0; j < size - i -1; ++j)
+		for (j = 0; j < size - 1; ++j)
 		{
 			if (array[j] > array[j + 1])
 			{

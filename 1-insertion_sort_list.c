2fadd6df4ba7afabab6881d2254cf0841650071a
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-24 19:12:50 -0700

    0x1B: Task1 C source code, Header and Big O Files

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * order using Insertion Sort algortihm.
+ *
+ * @listint_t: Pointer to an array of pointers to listint_t defined datatype
+ * which represents the integer values through a doubly linked list data
+ * structure.
+ *
+ * Return: Nothing.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	size_t i, list_length;
+	int j;
+	listint_t *to_insert, *to_compare;
+
+	if (*list == NULL)
+		return;
+
+	list_length = dlistint_len(*list);
+
+	if (list_length < 2)
+		return;
+
+	for (i = 1; i < list_length; i++)
+	{
+		to_insert = get_dnodeint_at_index(*list, i);
+
+		for (j = i - 1; j >= 0; j--)
+		{
+			to_compare = get_dnodeint_at_index(*list, j);
+			if (to_insert->n < to_compare->n)
+			{
+				to_insert->prev = to_compare->prev;
+				if (to_compare->prev != NULL)
+					to_compare->prev->next = to_insert;
+				to_compare->next = to_insert->next;
+				if (to_insert->next != NULL)
+					to_insert->next->prev = to_compare;
+				to_insert->next = to_compare;
+				to_compare->prev = to_insert;
+				if (j == 0)
+					*list = to_insert;
+
+				print_list(*list);
+			}
+			else if (to_insert->n >= to_compare->n)
+				break;
+		}
+	}
+}
---
+++ dlistint_funcs.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+
+/**
+ * dlistint_len - Returns the number of elements in a linked `listint_t`
+ * list.
+ *
+ * @h: Pointer to a Constant `listint_t` list, which represents the header
+ * node of the doubly linked list.
+ *
+ * Return: Number of nodes
+ */
+
+size_t dlistint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * get_dnodeint_at_index - Returns the nth node of a `listint_t` linked list.
+ *
+ * @head: Pointer to a `listint_t` list type, represents the head of the
+ * doubly linked list.
+ * @index: Unsigned Integer value, index of the node to be returned.
+ *
+ * Return: Address of the nth node (Success)
+ *	   NULL (Failure).
+ */
+listint_t *get_dnodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *curr = head;
+
+	while (count != index && curr != NULL)
+	{
+		curr = curr->next;
+		count++;
+	}
+
+	if (count != index)
+		return (NULL);
+
+	return (curr);
+}
+
+
+/**
+ * free_dlistint  - Frees a `listint_t` list.
+ *
+ * @head: Pointer to a `listint_t` list type, which represents the
+ *
+ * Return: Nothing.
+ */
+void free_dlistint(listint_t *head)
+{
+	listint_t *curr;
+
+	while (head != NULL)
+	{
+		curr = head;
+		head = head->next;
+		free(curr);
+	}
+}

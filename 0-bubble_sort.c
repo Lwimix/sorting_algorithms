dadb1bd479816be7fca62f7f1464fd365fad4230
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-25 18:52:23 -0700

    0x1B: Task0 C source code 1st Fix
    Removed the extra condition, and adjusted
    the incrementation.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,15 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp = 0, counter = 0;
+	int temp = 0;
 	unsigned int i = 0, j = 0;
 
-	if (array == NULL || size <= 0 || size < 2)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; ++i)
+
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1; ++j)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -24,13 +25,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
-				++counter;
 			}
-		}
-		if (counter == 0)
-		{
-			print_array(array, size);
-			break;
 		}
 	}
 }
